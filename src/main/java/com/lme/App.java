69e3daafa12c88bbd0a6c5dcbffccff36bc13f36
rajeshsomasundaram <rajesh.somasundaram@10xbanking.com>
2020-03-10 10:04:53 +0000

    Replaced the App with interface

--- src/main/java/com/lme/App.java
+++ src/main/java/com/lme/App.java
@@ -4,7 +4,7 @@ public class App {
 
   public static void main(String[] args) throws Exception {
     Grid grid = new Grid(5, 3);
-    BasicRobot robot1 = new BasicRobot(1, grid, grid.getCoordinate(1, 1), Orientation.id('E'));
+    Robot robot1 = new BasicRobot(1, grid, grid.getCoordinate(1, 1), Orientation.id('E'));
     //RFRFRFRF
     robot1.move(Direction.id('R'));
     robot1.move(Direction.id('F'));
@@ -14,7 +14,7 @@ public class App {
     robot1.move(Direction.id('F'));
     robot1.move(Direction.id('R'));
     robot1.move(Direction.id('F'));
-    BasicRobot robot2 = new BasicRobot(2, grid, grid.getCoordinate(3, 2), Orientation.id('N'));
+    Robot robot2 = new BasicRobot(2, grid, grid.getCoordinate(3, 2), Orientation.id('N'));
     //FRRFLLFFRRFLL
     robot2.move(Direction.id('F'));
     robot2.move(Direction.id('R'));
